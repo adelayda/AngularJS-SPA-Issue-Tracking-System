4ab3ab12964826d321fc3287297448af9d73cb3b
adelayda <adelina.yanakieva@gmail.com>
2016-04-13 11:20:25 +0300

    Added mainData.js

--- AngularJS-SPA-Issue-Tracking-System/app/js/services/mainData.js
+++ AngularJS-SPA-Issue-Tracking-System/app/js/services/mainData.js
@@ -1,3 +1,40 @@
-/**
- * Created by Adi on 4/13/2016.
- */
+'use strict';
+
+issueTracker.factory('mainData', function ($http, baseServiceUrl) {
+    var data = {};
+
+    data.params = {};
+
+    data.getAllProjects = function (success, error) {
+        $http.get(baseServiceUrl + '/Projects', {params: this.params})
+            .success(function (data) {
+                success(data)
+            }).error(error);
+    };
+
+
+   // data.getAllTowns = function (success, error) {
+   //     $http.get(baseServiceUrl + '/towns')
+   //         .success(function (data) {
+   //             success(data)
+   //         }).error(error);
+   // };
+
+   // data.getAllCategories = function (success, error) {
+   //     $http.get(baseServiceUrl + '/categories')
+   //         .success(function (data) {
+   //             success(data)
+   //         }).error(error);
+   // };
+
+    data.clearParams = function () {
+        data.params.categoryId = null;
+        data.params.townId = null;
+        data.params.startPage = 1;
+    };
+
+    data.clearParams();
+    data.params.pageSize = 5;
+
+    return data;
+});
