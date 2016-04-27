9d53ca15948c1c1e164acf003c8f29ce5cadf347
adelayda <adelina.yanakieva@gmail.com>
2016-04-27 15:56:24 +0300

    Edited app.js

--- AngularJS-SPA-Issue-Tracking-System/app/js/app.js
+++ AngularJS-SPA-Issue-Tracking-System/app/js/app.js
@@ -1,122 +1,71 @@
 'use strict';
 
-var issueTracker = angular.module('issueTracker', ['ngRoute', 'ui.bootstrap', 'angular-loading-bar']);
+var issueTracker = angular.module('IssueTrackingSystem', [
+    'ngRoute',
+    'ngResource',
+    'ui.bootstrap.pagination'
+])
+    .constant('baseServiceUrl', 'http://softuni-issue-tracker.azurewebsites.net/')
+    .constant('pageSize', 6)
+    .config(function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'templates/user/dashboard.html',
+            controller: 'AuthenticationController'
+        });
 
-issueTracker.constant('baseServiceUrl', 'http://softuni-issue-tracker.azurewebsites.net');
+        $routeProvider.when('/user/profile/password', {
+            templateUrl: 'templates/user/edit-password.html',
+            controller: 'UserEditProfileController'
+        });
 
-issueTracker.config(function ($routeProvider) {
-    $routeProvider
-        .when('/login', {
-            templateUrl:'templates/login.html',
-            controller:'MainController'
-        })
-        .when('/register', {
-            templateUrl:'templates/register.html',
-            controller:'MainController'
-        })
-        .when('/', {
-              templateUrl: 'templates/user/dashboard.html',
-              controller: 'AuthenticationController'
-        })
-        .when('/user/profile/password', {
-              templateUrl: 'templates/user/edit-password.html',
-              controller: 'UserEditProfileController'
-        })
-        .when('/projects', {
-              templateUrl: 'templates/projects/all-projects.html',
-              controller: 'AdminController'
-        })
-        .when('/projects/add', {
-              templateUrl: 'templates/projects/add-new-project.html',
-              controller: 'AdminController'
-        })
-        .when('/projects/:id', {
-              templateUrl: 'templates/projects/project-page.html',
-              controller: 'ViewProjectController'
-        })
-        .when('/projects/:id/add-issue', {
-              templateUrl: 'templates/issues/add-new-issue.html',
-              controller: 'ProjectsController'
-        })
-        .when('/projects/:id/edit', {
-              templateUrl: 'templates/projects/edit-project.html',
-              controller: 'EditProjectController'
-        })
-        .when('/issues/:id', {
-              templateUrl: 'templates/issues/issue-page.html',
-              controller: 'IssuesController'
-        })
-        .when('/issues/:id/edit', {
-              templateUrl: 'templates/issues/edit-issue.html',
-              controller: 'EditIssueController'
-        })
+        $routeProvider.when('/projects', {
+            templateUrl: 'templates/projects/all-projects.html',
+            controller: 'AdminController'
+        });
 
-        .otherwise({redirectTo: '/'});
-});
+        $routeProvider.when('/projects/add', {
+            templateUrl: 'templates/projects/add-new-project.html',
+            controller: 'AdminController'
+        });
 
-issueTracker.run(function ($rootScope, $location, authentication) {
+        $routeProvider.when('/projects/:id', {
+            templateUrl: 'templates/projects/project-page.html',
+            controller: 'ViewProjectController'
+        });
+
+        $routeProvider.when('/projects/:id/add-issue', {
+            templateUrl: 'templates/issues/add-new-issue.html',
+            controller: 'ProjectsController'
+        });
+
+        $routeProvider.when('/projects/:id/edit', {
+            templateUrl: 'templates/projects/edit-project.html',
+            controller: 'EditProjectController'
+        });
+
+        $routeProvider.when('/issues/:id', {
+            templateUrl: 'templates/issues/issue-page.html',
+            controller: 'IssuesController'
+        });
+
+        $routeProvider.when('/issues/:id/edit', {
+            templateUrl: 'templates/issues/edit-issue.html',
+            controller: 'EditIssueController'
+        });
+
+        $routeProvider.otherwise(
+            {
+                redirectTo: '/'
+            }
+        );
+    });
+
+issueTracker.run(function ($rootScope, $location, authenticationService) {
     $rootScope.$on('$locationChangeStart', function (event) {
-        if (!authentication.isLoggedIn()) {
-          // Authorization check: anonymous site visitors cannot access user routes
-          $location.path("/");
-      }
+        if (!authenticationService.isLoggedIn()) {
+            // Authorization check: anonymous site visitors cannot access user routes
+            $location.path("/");
+        }
     });
 });
-
-
-        //
-        //
-        // Login Screen
-        //    Route: #/
-        // Register User Screen - After registration, the user is automatically logged in and is redirected to the dashboard.
-        //  Route: #/
-        //
-        // User Dashboard
-        // Route: #/
-        //
-        // Project Page
-        // Route: #/projects/:id
-        //
-        // Edit Project Page
-        // Route: #/projects/:id/edit
-        //
-        // Add Issue
-        // Route: #/projects/:id/add-issue
-        //
-        // Issue page
-        // Route: #/issues/:id
-        //
-        // Edit Issue page
-        // Route: #/issues/:id/edit
-        //
-        // Change User Password
-        // Route: #/profile/password
-        //
-        // Logout
-        // Route: #/logout
-        //
-        // Guest Authorization Checks
-        // Anonymous site visitors (without login) should be able to access only Login and Register screens.
-
-
-        // Projects/?pageSize={pageSize}&pageNumber={pageNumber}&{filter}={value}
-        // Projects/{id}
-        // Projects/
-        //  Projects/{id}/Issues
-        //  Issues/?pageSize={pageSize}&pageNumber={pageNumber}&{filter}={value}
-        //  Issues/me?pageSize={pageSize}&pageNumber={pageNumber}&orderBy={by}
-        //  Issues/{id}
-        //  Issues/
-        //  Issues/{id}
-        //  Issues/{id}/changestatus?statusid={statusId}
-        //  Issues/{id}/comments
-        //  Issues/{id}/comments
-        //  Labels/?filter={filter}
-        //  api/Account/Register
-        //  api/Token
-        //  Users/
-        //  Users/me
-        //  Users/makeadmin
-        //  api/Account/ChangePassword
-
 
